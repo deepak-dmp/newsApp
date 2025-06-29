fb20c2881ed991255645a2cb398cccd2e1c78366
deepak-dmp <93182222+deepak-dmp@users.noreply.github.com>
2025-06-30 02:02:12 +0530

    video 22: craeted new component , navabr and news item component

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import React, { Component } from 'react'
+import NavBar from './Components/NavBar';
+import News from './Components/News';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        {/* //07abc97fe06a411bba409f74b9240dc5 */}
+        <NavBar/>
+        <News/>
+      </div>
+    )
+  }
+}
