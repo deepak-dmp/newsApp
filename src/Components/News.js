bda81b4131a827b8ba5fcea1086ada11ee09aa60
deepak-dmp <93182222+deepak-dmp@users.noreply.github.com>
2025-07-01 07:01:23 +0530

    video #28:- Added next and previous button also added  diabled for previous if we are at page 1 and added logic for next page up till last page with math.ceil method on totalResults also i added alert for last page as 'all news covered'

--- src/Components/News.js
+++ src/Components/News.js
@@ -8,18 +8,49 @@ export default class News extends Component {
     super();
     this.state={
       articles: [ ],
-      loadig:false
+      loadig:false,
+      page:1
     }
   }
 
    async componentDidMount(){
     console.log("componentdidmount");
-    let url = "https://newsapi.org/v2/top-headlines?category=sports&apiKey=07abc97fe06a411bba409f74b9240dc5"
+    let url = "https://newsapi.org/v2/top-headlines?category=sports&apiKey=07abc97fe06a411bba409f74b9240dc5&page=1&pageSize=18"
     let data = await fetch(url);
     let parsedData=await data.json();
     console.log(parsedData);
-    this.setState({articles: parsedData.articles})
+    this.setState({articles: parsedData.articles,totalResults:parsedData.totalResults})
     
+  }
+
+   handleNextClick = async ()=>{
+    if(this.state.page+1>Math.ceil(this.state.totalResults/18)){
+     
+      alert("all News covered ")
+    }
+    else{
+    let url = `https://newsapi.org/v2/top-headlines?category=sports&apiKey=07abc97fe06a411bba409f74b9240dc5&page=${this.state.page+1}&pageSize=18`
+    let data = await fetch(url);
+    let parsedData=await data.json();
+    console.log(parsedData);
+    this.setState({
+      articles: parsedData.articles,
+      page:this.state.page+1
+    })
+    }
+
+  }
+
+  handlePrevClick = async()=>{
+    let url = `https://newsapi.org/v2/top-headlines?category=sports&apiKey=07abc97fe06a411bba409f74b9240dc5&page=${this.state.page-1}&pageSize=18`
+    let data = await fetch(url);
+    let parsedData=await data.json();
+    console.log(parsedData);
+    this.setState({
+      articles: parsedData.articles,
+      page:this.state.page-1
+    })
+
   }
 
   render() {
@@ -39,7 +70,11 @@ export default class News extends Component {
         
         
         </div>
-        
+        <div className='container d-flex justify-content-between'>
+          <button type="button" disabled={this.state.page<=1} className="btn btn-dark" onClick={this.handlePrevClick}>&larr; Preview</button>
+          <button type="button" className="btn btn-dark" onClick={this.handleNextClick}>Next &rarr;</button>
+           
+        </div>
 
       </div>
     )
